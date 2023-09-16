b1f9c0bc3e3a6db369d729a50a90a78fb5bb5335
CaibalLester <caiballester4@gmail.com>
2023-09-16 20:15:56 +0800

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/CaibalLester', 'Home::CaibalLester');
+$routes->get('/product', 'ProductController::CaibalLester');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
+
